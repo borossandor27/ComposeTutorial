2c0dfaa2cb4f44e592097d41f160104129c84115
borossandor27 <boros.sanyi@gmail.com>
2024-06-29 17:08:05 +0200

    MateriaTeme

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -7,42 +7,68 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+
 //-- forrás: https://developer.android.com/develop/ui/compose/tutorial
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MessageCard(Message("Android","Jetpack Compose"))
+            ComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
         }
     }
 }
+
 data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row {
-        Image(painter = painterResource(id = R.drawable.profile_picture), contentDescription = "Contact profile picture",
-            modifier = Modifier.padding(all = 16.dp))
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(MaterialTheme.shapes.small)
+        )
+        //-- Add horizontal space between the image and the column
+        Spacer(modifier = Modifier.size(8.dp))
     }
     Column {
         Text(text = msg.author)
+        Spacer(modifier = Modifier.size(4.dp))  // Add a vertical space between the author and message
         Text(text = msg.body)
     }
 
 }
+
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!"))
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!"))
+        }
     }
+}
